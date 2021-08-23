01cc2711c0a676dc5a85b370eb673bfd2ac0bf4e
sunny <sunnyvales789@gmail.com>
2021-08-23 23:19:39 +0530

    Update test.js

--- test.js
+++ test.js
@@ -273,3 +273,79 @@ return this.followers.map(
     let ibj = new User('a', ['1', '2', '3', '4', '5']);
     ibj.addFollower('b')
     console.log(ibj.getFollowers('2'));
+
+
+-------------------------
+ 
+ 
+Q.1.
+Consider the two functions below. Will they both return the same thing? Why or why not?
+function foo1()
+{
+  return {
+      bar: "hello"
+  };
+}
+
+function foo2()
+{
+  return
+  {
+      bar: "hello"
+  };
+}
+
+Q.2
+In what order will the numbers 1-4 be logged to the console when the code below is executed? Why?
+(function() {
+    console.log(1); 
+    setTimeout(function(){console.log(2)}, 1000); 
+    setTimeout(function(){console.log(3)}, 0); 
+    console.log(4);
+})();
+
+Q.3
+Write a sum method which will work properly when invoked using either syntax below.
+console.log(sum(2,3));   // Outputs 5
+console.log(sum(2)(3));  // Outputs 5
+
+Q.4
+What will the code below output to the console and why?
+var arr1 = "john".split('');
+var arr2 = arr1.reverse();
+var arr3 = "jones".split('');
+arr2.push(arr3);
+console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+Q.5
+What is the output of the following code?
+var length = 10;
+function fn() {
+	console.log(this.length);
+}
+
+var obj = {
+  length: 5,
+  method: function(fn) {
+    fn();
+    arguments[0]();
+  }
+};
+
+obj.method(fn, 1);
+
+Q.6
+What would following code return?
+console.log(typeof typeof 1);
+
+Q.8
+duplicate([1, 2, 3, 4, 5]); // [1,2,3,4,5,1,2,3,4,5]
+
+Q.7 How to check if an object is an array or not? Provide some code
+
+Q.9
+write a http service method where i can call N number of requestes with the same function using AXIOS (method GET)
+Need a textarea box with a random text
+when i clicked on button need to copy of that text in side the text area and paste it out in another div
+if i changed the text in text area and again if i clicked on button need to update the text in side div
